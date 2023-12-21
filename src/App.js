063569ae24b1343957e160bdff07b9dad40bc02a
ashwin4638 <guptaashwin4638@gmail.com>
2023-12-21 19:43:11 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext } from "react";
 
 // Create context to store projects and tasks
 const TimeTrackerContext = createContext();
@@ -11,7 +11,7 @@ function App() {
   function handleProjectSubmit(e) {
     e.preventDefault();
     const name = e.target.elements.name.value;
-    setProjects(prevProjects => [...prevProjects, { id: Date.now(), name }]);
+    setProjects((prevProjects) => [...prevProjects, { id: Date.now(), name }]);
     e.target.reset();
   }
 
@@ -22,7 +22,10 @@ function App() {
     const time = e.target.elements.time.value;
     const description = e.target.elements.description.value;
     const date = new Date().toLocaleDateString();
-    setTasks(prevTasks => [...prevTasks, { id: Date.now(), project, name, time, description, date }]);
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: Date.now(), project, name, time, description, date },
+    ]);
     e.target.reset();
   }
 
@@ -33,38 +36,52 @@ function App() {
   // Calculate total hours covered for the current day
   const today = new Date().toLocaleDateString();
   const totalHoursToday = tasks
-    .filter(task => task.project === selectedProject && task.date === today)
+    .filter((task) => task.project === selectedProject && task.date === today)
     .reduce((total, task) => total + parseFloat(task.time), 0);
 
   return (
     <TimeTrackerContext.Provider value={{ projects, tasks }}>
       <div>
-        <h1>Time Tracking App</h1>
+        <h1>Time Tracking App React</h1>
         <form onSubmit={handleProjectSubmit}>
           <input type="text" name="name" placeholder="Enter project name" />
           <button type="submit">Create Project</button>
         </form>
-        <ul className='Projects'>
-          {projects.map(project => (
-            <li className='Project-List' key={project.id} onClick={() => handleProjectSelect(project.id)}>
+        <ul className="Projects">
+          {projects.map((project) => (
+            <li
+              className="Project-List"
+              key={project.id}
+              onClick={() => handleProjectSelect(project.id)}
+            >
               {project.name}
             </li>
           ))}
         </ul>
         {selectedProject && (
           <div>
-            <h2>Selected Project: {projects.find(project => project.id === selectedProject).name}</h2>
+            <h2>
+              Selected Project:{" "}
+              {projects.find((project) => project.id === selectedProject).name}
+            </h2>
             <form onSubmit={handleTaskSubmit}>
               <input type="text" name="name" placeholder="Enter task name" />
               <input type="number" name="time" placeholder="Enter time spent" />
-              <input type="text" name="description" placeholder="Enter task description" />
+              <input
+                type="text"
+                name="description"
+                placeholder="Enter task description"
+              />
               <button type="submit">Create Task</button>
             </form>
             <h3>Total Hours Today: {totalHoursToday}</h3>
             <ul>
               {tasks
-                .filter(task => task.project === selectedProject && task.date === today )
-                .map(task => (
+                .filter(
+                  (task) =>
+                    task.project === selectedProject && task.date === today
+                )
+                .map((task) => (
                   <li key={task.id}>
                     <p>{`Task Name: ${task.name}`}</p>
                     <p>{`Task Time: ${task.time} hours`}</p>
